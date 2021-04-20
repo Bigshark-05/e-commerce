dbfb5702a3511de84c79491962a5cc507b611dd9
Bigshark-05 <gwen.moel@hotmail.fr>
2021-04-20 22:56:58 +0200

    animation

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,33 +1,35 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand">
-      <img src="../assets/logo.png" width="30" height="30" />
-      Dyma
-    </a>
-    <button class="navbar-toggler" v-trigger-collapse="'collapse'">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div id="collapse" class="collapse navbar-collapse">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            :class="{ active: page === 'User' }"
-            @click="changePage('User')"
-            >Boutique</a
-          >
-        </li>
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            :class="{ active: page === 'Admin' }"
-            @click="changePage('Admin')"
-            >Admin</a
-          >
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <transition appear>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand">
+        <img src="../assets/logo.png" width="30" height="30" />
+        Dyma
+      </a>
+      <button class="navbar-toggler" v-trigger-collapse="'collapse'">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div id="collapse" class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              :class="{ active: page === 'User' }"
+              @click="changePage('User')"
+              >Boutique</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              :class="{ active: page === 'Admin' }"
+              @click="changePage('Admin')"
+              >Admin</a
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </transition>
 </template>
 
 <script>
@@ -73,5 +75,16 @@ export default {
 <style scoped>
 a {
   cursor: pointer;
+}
+
+@keyframes fromtop {
+  from {
+    transform: translateY(-20px);
+  }
+  to {
+  }
+}
+.v-enter-active {
+  animation: fromtop 1s;
 }
 </style>
